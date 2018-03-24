2fc79bb7c773fd6409125af9b329714ce8e5c624
Omer Akram <om26er@gmail.com>
2018-03-25 00:44:31 +0500

    More towards a rock solid always-on service

--- mobile/src/main/java/network/xbr/xbrisgold/LongRunningService.java
+++ mobile/src/main/java/network/xbr/xbrisgold/LongRunningService.java
@@ -2,7 +2,10 @@ package network.xbr.xbrisgold;
 
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -19,29 +22,44 @@ import io.crossbar.autobahn.wamp.types.ExitInfo;
 import io.crossbar.autobahn.wamp.types.SessionDetails;
 
 public class LongRunningService extends Service {
+    
+    private Session mWAMPSession;
+
+    private BroadcastReceiver mNetworkStateChangeListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (!Helpers.isNetworkAvailable(context)) {
+                return;
+            }
+            // Maybe ping a server then try to connect ?
+        }
+    };
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        System.out.println("Starting service");
+        mWAMPSession = new Session();
         connectToServer().whenComplete((exitInfo, throwable) -> System.out.println(exitInfo));
+        registerReceiver(mNetworkStateChangeListener,
+                new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
         // Automatically restarts the service, if killed by the OS.
         return START_STICKY;
     }
 
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mNetworkStateChangeListener);
+        super.onDestroy();
+    }
+
     private CompletableFuture<ExitInfo> connectToServer() {
-        Session wampSession = new Session();
-        wampSession.addOnJoinListener(this::onJoin);
-        wampSession.addOnLeaveListener((session, closeDetails) -> {
-            System.out.println("LEFT");
-        });
-        wampSession.addOnDisconnectListener((session, b) -> {
-            System.out.println("DISCONNECTED");
-        });
+        mWAMPSession.addOnJoinListener(this::onJoin);
+        mWAMPSession.addOnLeaveListener((session, closeDetails) -> System.out.println("LEFT"));
+        mWAMPSession.addOnDisconnectListener((session, b) -> System.out.println("DISCONNECTED"));
         IAuthenticator auth = new CryptosignAuth(
                 "test@crossbario.com",
                 "ef83d35678742e01fa412d597cd3909c113b12a8a7dc101cba073c0423c9db41",
                 "e5b0d24af05c77d644de885946147aeb4fa6897a5cf4eb14347c3d637664b117");
-        Client client = new Client(wampSession, "ws://178.62.69.210:8080/ws", "realm1", auth);
+        Client client = new Client(mWAMPSession, "ws://178.62.69.210:8080/ws", "realm1", auth);
         return client.connect();
     }
 
