a907fb4a94313a9c22fb9c4c5e800407eb85405b
Omer Akram <om26er@gmail.com>
2018-03-25 01:56:47 +0500

    start the service without checking network as service itself checks it

--- mobile/src/main/java/network/xbr/xbrisgold/BootCompletedReceiver.java
+++ mobile/src/main/java/network/xbr/xbrisgold/BootCompletedReceiver.java
@@ -3,18 +3,13 @@ package network.xbr.xbrisgold;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
         String action = intent.getAction();
         if (action != null && action.equals(Intent.ACTION_BOOT_COMPLETED)) {
-            if (Helpers.isNetworkAvailable(ctx.getApplicationContext())) {
-                ctx.startService(new Intent(ctx.getApplicationContext(), LongRunningService.class));
-            } else {
-                Toast.makeText(ctx.getApplicationContext(), "Network unavailable", Toast.LENGTH_SHORT).show();
-            }
+            ctx.startService(new Intent(ctx.getApplicationContext(), LongRunningService.class));
         }
     }
 }
