d92bfc5d8470b748206828133566299c484e0a19
Omer Akram <om26er@gmail.com>
2018-03-25 01:54:07 +0500

    attempt auto-reconnect in case we don't have a working internet

--- mobile/src/main/java/network/xbr/xbrisgold/LongRunningService.java
+++ mobile/src/main/java/network/xbr/xbrisgold/LongRunningService.java
@@ -6,10 +6,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -18,30 +18,22 @@ import io.crossbar.autobahn.wamp.Client;
 import io.crossbar.autobahn.wamp.Session;
 import io.crossbar.autobahn.wamp.auth.CryptosignAuth;
 import io.crossbar.autobahn.wamp.interfaces.IAuthenticator;
-import io.crossbar.autobahn.wamp.types.ExitInfo;
 import io.crossbar.autobahn.wamp.types.SessionDetails;
 
 public class LongRunningService extends Service {
-    
-    private Session mWAMPSession;
 
     private BroadcastReceiver mNetworkStateChangeListener = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (!Helpers.isNetworkAvailable(context)) {
-                return;
-            }
-            // Maybe ping a server then try to connect ?
+            actuallyConnect(context);
         }
     };
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        mWAMPSession = new Session();
-        connectToServer().whenComplete((exitInfo, throwable) -> System.out.println(exitInfo));
         registerReceiver(mNetworkStateChangeListener,
                 new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
-        // Automatically restarts the service, if killed by the OS.
+        // Automatically restarts the service if killed by the OS.
         return START_STICKY;
     }
 
@@ -51,16 +43,39 @@ public class LongRunningService extends Service {
         super.onDestroy();
     }
 
-    private CompletableFuture<ExitInfo> connectToServer() {
-        mWAMPSession.addOnJoinListener(this::onJoin);
-        mWAMPSession.addOnLeaveListener((session, closeDetails) -> System.out.println("LEFT"));
-        mWAMPSession.addOnDisconnectListener((session, b) -> System.out.println("DISCONNECTED"));
+    private void actuallyConnect(Context ctx) {
+        if (Helpers.isNetworkAvailable(ctx.getApplicationContext())) {
+            Helpers.isInternetWorking().whenComplete((working, throwable) -> {
+                if (working) {
+                    connectToServer();
+                } else {
+                    // Network is available but we don't have a working internet
+                    // lets schedule something to recheck if internet is working.
+                    new Handler().postDelayed(() -> actuallyConnect(ctx), 10000);
+                }
+            });
+        }
+    }
+
+    private void connectToServer() {
+        Session wampSession = new Session();
+        wampSession.addOnJoinListener(this::onJoin);
+        wampSession.addOnLeaveListener((session, closeDetails) -> System.out.println("LEFT"));
+        wampSession.addOnDisconnectListener((session, b) -> {
+            // Check if we have a working internet before attempting a reconnect.
+            System.out.println("DISCONNECTED");
+            actuallyConnect(getApplicationContext());
+        });
         IAuthenticator auth = new CryptosignAuth(
                 "test@crossbario.com",
                 "ef83d35678742e01fa412d597cd3909c113b12a8a7dc101cba073c0423c9db41",
                 "e5b0d24af05c77d644de885946147aeb4fa6897a5cf4eb14347c3d637664b117");
-        Client client = new Client(mWAMPSession, "ws://178.62.69.210:8080/ws", "realm1", auth);
-        return client.connect();
+        Client client = new Client(wampSession, "ws://178.62.69.210:8080/ws", "realm1", auth);
+        client.connect().whenComplete((exitInfo, throwable) -> {
+            if (throwable != null) {
+                throwable.printStackTrace();
+            }
+        });
     }
 
     private void onJoin(Session session, SessionDetails details) {
@@ -69,8 +84,7 @@ public class LongRunningService extends Service {
         executorService.scheduleAtFixedRate(() -> {
             session.publish("io.xbr.gold.tick", i[0]).whenComplete((publication, throwable) -> {
                 if (throwable == null) {
-                    System.out.println(String.format(
-                            "Published io.cbr.gold.tick, ID: %s", publication.publication));
+                    System.out.println(String.format("Published io.cbr.gold.tick %s", i[0]));
                 }
             });
             i[0]++;
