f09594059349eb694b8b8dfb546cd4dfbe8411c1
Omer Akram <om26er@gmail.com>
2018-03-26 02:41:21 +0500

    Create a connection queue to avoid duplicate connections to crossbar

--- mobile/src/main/java/network/xbr/xbrisgold/MainActivity.java
+++ mobile/src/main/java/network/xbr/xbrisgold/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
             // Show dialog that app may get killed in the background by the OS
             // and won't work once device enters doze mode.
             // https://developer.android.com/training/monitoring-device-state/doze-standby.html
+            startService(new Intent(getApplicationContext(), LongRunningService.class));
         }
     }
 }
