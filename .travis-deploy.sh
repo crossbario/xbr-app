3a01fb8a3e91a69f1be9c1097c697d34309cc6f4
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-05-19 20:43:55 +0200

    sync (test/trigger ci) - Sa 19. Mai 20:43:55 CEST 2018

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-#tox -c tox.ini -e buildexe
-aws s3 cp --acl public-read --recursive ./build/outputs/apk/release/ s3://${AWS_S3_BUCKET_NAME}/xbr-is-gold/android/
-#aws s3api copy-object --acl public-read --copy-source ${AWS_S3_BUCKET_NAME}/crossbarfx/linux-amd64/crossbarfx-${CROSSBAR_BUILD_ID} --bucket ${AWS_S3_BUCKET_NAME} --key crossbarfx/linux-amd64/crossbarfx-latest
+# ./mobile/build/generated/res/microapk/release/raw/android_wear_micro_apk.apk
+# ./mobile/build/outputs/apk/debug/mobile-debug.apk
+# ./mobile/build/outputs/apk/androidTest/debug/mobile-debug-androidTest.apk
+# ./mobile/build/outputs/apk/release/mobile-release-unsigned.apk
+# ./wear/build/outputs/apk/debug/wear-debug.apk
+# ./wear/build/outputs/apk/androidTest/debug/wear-debug-androidTest.apk
+# ./wear/build/outputs/apk/release/wear-release-unsigned.apk
+
+# android mobile
+#
+aws s3 cp --acl public-read ./mobile/build/outputs/apk/release/mobile-release-unsigned.apk s3://${AWS_S3_BUCKET_NAME}/xbr-is-gold/android-mobile/xbr-is-gold-${CROSSBAR_BUILD_ID}.apk
+aws s3api copy-object --acl public-read --copy-source ${AWS_S3_BUCKET_NAME}/xbr-is-gold/android-mobile/xbr-is-gold-${CROSSBAR_BUILD_ID}.apk --bucket ${AWS_S3_BUCKET_NAME} --key /xbr-is-gold/android-mobile/xbr-is-gold-latest.apk
+
+# android wear
+#
+aws s3 cp --acl public-read ./wear/build/outputs/apk/release/wear-release-unsigned.apk s3://${AWS_S3_BUCKET_NAME}/xbr-is-gold/android-wear/xbr-is-gold-${CROSSBAR_BUILD_ID}.apk
+aws s3api copy-object --acl public-read --copy-source ${AWS_S3_BUCKET_NAME}/xbr-is-gold/android-wear/xbr-is-gold-${CROSSBAR_BUILD_ID}.apk --bucket ${AWS_S3_BUCKET_NAME} --key /xbr-is-gold/android-wear/xbr-is-gold-latest.apk
