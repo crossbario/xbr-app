51fdc5c54613572edd90cc8d4fa6c8a253fc0759
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-05-19 20:32:32 +0200

    sync (test/trigger ci) - Sa 19. Mai 20:32:32 CEST 2018

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #tox -c tox.ini -e buildexe
-aws s3 cp --acl public-read --recursive /home/travis/build/xbr/xbr-is-gold/app/build/outputs/apk/release/ s3://${AWS_S3_BUCKET_NAME}/xbr-is-gold/android/
+aws s3 cp --acl public-read --recursive ./build/outputs/apk/release/ s3://${AWS_S3_BUCKET_NAME}/xbr-is-gold/android/
 #aws s3api copy-object --acl public-read --copy-source ${AWS_S3_BUCKET_NAME}/crossbarfx/linux-amd64/crossbarfx-${CROSSBAR_BUILD_ID} --bucket ${AWS_S3_BUCKET_NAME} --key crossbarfx/linux-amd64/crossbarfx-latest
